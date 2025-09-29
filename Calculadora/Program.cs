d20a0affd6b1c9949cc9ac9fb4837572376c8cea
Jaqezita <jaque_hp@hotmail.com>
2025-09-28 22:48:22 -0300

    feat: imprime a lista completa de operações após cada cálculo

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -20,13 +20,44 @@ namespace Calculadora
             
             while (filaOperacoes.Count > 0) // Condição ajustada para garantir que o loop finalize quando a fila estiver vazia.
             {
-                Operacoes operacao = filaOperacoes.Dequeue(); // Metódo corrigido para que os elementos seja retirados da fila após processamento.
-                calculadora.calcular(operacao);
-                Console.WriteLine("{0} {1} {2} = {3}", operacao.valorA,operacao.operador,operacao.valorB, operacao.resultado);
-            }
+                // Implementar tratamento de exceção para divisão por zero
+                try
+                {
+                    Operacoes operacao = filaOperacoes.Dequeue(); // Metódo corrigido para que os elementos seja retirados da fila após processamento.
+                    calculadora.calcular(operacao);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Operação processada:");
+                    Console.ResetColor();
+                    Console.WriteLine("{0} {1} {2} = {3}\n", operacao.valorA, operacao.operador, operacao.valorB, operacao.resultado);
+                    //Imprimir a lista de operações a ser processada após do cálculo
+                    if (filaOperacoes.Count > 0)
+                    {
+                        ImprimirFilaOperacoes(filaOperacoes);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nenhuma operação restante na fila.\n");
+                    }
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
 
-          
-           
+            }
         }
+        private static void ImprimirFilaOperacoes(Queue<Operacoes> fila)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Próximas operações na fila: {0}", fila.Count);
+            Console.ResetColor(); 
+            foreach (var operacao in fila)
+            {
+                Console.WriteLine("{0} {1} {2}", operacao.valorA, operacao.operador, operacao.valorB);
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------\n");
+        }       
     }
 }
